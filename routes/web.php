d540e92123836e3f10c39183f23947c2f93b74f3
Leonardo Buarque <leonardobuarque14@gmail.com>
2021-11-18 20:16:43 -0300

    parametros na rota

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,12 @@ Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal']);
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class,'sobreNos']);
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class,'contato']);
 
+
+Route::get('/contato/{nome}/{categoria_id}',
+    function(
+        String $nome = 'sem nome', 
+        int $categoria_id = 1
+        ){
+        echo "$nome - $categoria_id";
+    }
+)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
