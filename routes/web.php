34fd6910ea1a6d9a770aa495d3ce60c4b015db39
Leonardo Buarque <leonardobuarque14@gmail.com>
2021-11-18 19:39:07 -0300

    controllers

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return "Olá mundo";
-});
+Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal']);
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class,'principal']);
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class,'principal']);
 
-Route::get('/sobre-nos', function () {
-    return "Sobre";
-});
-
-Route::get('/contato', function () {
-    return "Contato";
-});
