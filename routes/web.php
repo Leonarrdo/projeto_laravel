805b27af60a20cd44056dcd743b7a96519334359
Leonardo Buarque <leonardobuarque14@gmail.com>
2021-11-18 21:22:35 -0300

    nomeando rotas

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal']);
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class,'sobreNos']);
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class,'contato']);
+Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal'])->name('site.index');
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class,'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class,'contato'])->name('site.contato');
+Route::get('/login', function(){return 'login';})->name('site.login');
 
-
-Route::get('/contato/{nome}/{categoria_id}',
-    function(
-        String $nome = 'sem nome', 
-        int $categoria_id = 1
-        ){
-        echo "$nome - $categoria_id";
-    }
-)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){return 'clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){return 'fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
+});
